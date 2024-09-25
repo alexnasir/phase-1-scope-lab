347edb0acb9fb4f5591443e6595354837dee91be
alex <alexnasiali45@gmail.com>
2024-09-25 16:10:03 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+    function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    }
+
+    var BestCustomer;
+    function setBestCustomer() {
+        bestCustomer = 'not bob';
+    }
+    function overwriteBestCustomer(newCustomer) {
+        bestCustomer = newCustomer;
+    }
+    const leastfavoriteCustomer = 'sally';
+    function changeLeastfavoriteCustomer() {
+        leastfavoriteCustomer = 'alex';
+    }
+    function changeLeastfavoriteCustomer() {
+        try{
+        leastfavoriteCustomer = 'alex';
+        }catch (error) {
+            console.log(`Cannot reassign least favorite customer`, error.message);
+        }
+    }
